8bab29e4745da6bb641065ce2381b7ef428633f4
joao-pm-santos96 <joao.pm.santos96@gmail.com>
2022-07-09 12:01:21 +0100

    update map sizes

--- rm_cleaner_js/scripts/cleaner_bot.py
+++ rm_cleaner_js/scripts/cleaner_bot.py
@@ -7,9 +7,16 @@
 IMPORTS
 """
 import rospy
+import actionlib
 import numpy as np
 
+import random
+
 from nav_msgs.msg import OccupancyGrid
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from geometry_msgs.msg import Quaternion
+
+from tf.transformations import quaternion_from_euler
 
 """
 METADATA
@@ -44,7 +51,7 @@ class CleanerBot:
         self.cleaned_area = 0
         self.clean_ratio = 0
 
-    def area_map_callback(self, map):
+    def cleaned_map_callback(self, map):
         resolution = map.info.resolution
         cleaned_cells = np.sum(np.array(map.data) == COSTMAP_FREE)
 
@@ -57,9 +64,14 @@ class CleanerBot:
         # TODO get params        
         self.map_topic = "/map"
         self.cleaned_area_topic = "/cleaned_area/costmap/costmap"
+        self.move_base_ns = "move_base"
+        self.rate = rospy.Rate(5) #hz
 
         # Subscribe
-        rospy.Subscriber(self.cleaned_area_topic, OccupancyGrid, self.area_map_callback)
+        rospy.Subscriber(self.cleaned_area_topic, OccupancyGrid, self.cleaned_map_callback)
+
+        # Action client
+        self.move_client = actionlib.SimpleActionClient(self.move_base_ns, MoveBaseAction)
 
         rospy.loginfo("Bot configured")
 
@@ -76,6 +88,30 @@ class CleanerBot:
 
         rospy.loginfo(f"Total area: {self.total_area} m2")
 
+    def clean(self):
+        
+        self.move_client.wait_for_server()
+
+        goal = MoveBaseGoal()
+        goal.target_pose.header.frame_id = self.map_topic if self.map_topic[0] != "/" else self.map_topic[1:]
+
+        while True:
+            goal.target_pose.header.stamp = rospy.Time.now()
+
+            goal.target_pose.pose.position.x = random.uniform(-10, 10)
+            goal.target_pose.pose.position.y = random.uniform(-10, 10)
+            quat_tf = quaternion_from_euler(0, 0, random.uniform(0,2*3.14))
+            quat_msg = Quaternion(quat_tf[0], quat_tf[1], quat_tf[2], quat_tf[3])
+            goal.target_pose.pose.orientation = quat_msg
+
+            self.move_client.send_goal(goal)
+            wait = self.move_client.wait_for_result()
+
+            if not wait:
+                pass
+
+            self.rate.sleep()
+
 
 """
 FUNCTIONS DEFINITIONS
@@ -91,7 +127,7 @@ if __name__ == '__main__':
         bot = CleanerBot()
         bot.config()
         bot.get_map_details()
-
+        bot.clean()
 
         rospy.spin()
 
