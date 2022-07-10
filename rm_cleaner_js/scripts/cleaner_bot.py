ab74f50fe319503e889019bce3316ab64d0e3d07
joao-pm-santos96 <joao.pm.santos96@gmail.com>
2022-07-10 17:26:04 +0100

    new clean strategy

--- rm_cleaner_js/scripts/cleaner_bot.py
+++ rm_cleaner_js/scripts/cleaner_bot.py
@@ -10,6 +10,8 @@ import rospy
 import actionlib
 import numpy as np
 
+import tf2_ros
+import tf2_geometry_msgs
 import random
 
 from nav_msgs.msg import OccupancyGrid
@@ -66,7 +68,10 @@ class CleanerBot:
         self.map_topic = "/map"
         self.cleaned_area_topic = "/cleaned_area/costmap/costmap"
         self.move_base_ns = "move_base"
-        self.rate = rospy.Rate(5) #hz
+        self.clean_goal = 1.0
+        self.rate = rospy.Rate(15) #hz
+        self.footprint_w = 0.9 * 4
+        self.footprint_h = 0.6
 
         # Subscribe
         rospy.Subscriber(self.cleaned_area_topic, OccupancyGrid, self.cleaned_map_callback)
@@ -76,9 +81,12 @@ class CleanerBot:
 
         rospy.loginfo("Bot configured")
 
-    def get_map_details(self):
+    def get_infos(self):
+
         rospy.loginfo(f"Waiting for map on topic {self.map_topic}")
+
         occ_map = rospy.wait_for_message(self.map_topic, OccupancyGrid)
+        
         rospy.loginfo(f"Map received")
 
         self.resolution = occ_map.info.resolution
@@ -86,52 +94,154 @@ class CleanerBot:
         self.map_h  = occ_map.info.height
         self.map_origin = occ_map.info.origin
 
-        # self.map = np.array(occ_map.data)
         self.map = np.asarray(occ_map.data, dtype=np.int8)#.reshape(occ_map.info.height, occ_map.info.width)
         free_cells = np.count_nonzero(self.map == COSTMAP_FREE)
         self.total_area = free_cells * (self.resolution ** 2)
 
         rospy.loginfo(f"Total area: {self.total_area} m2")
 
+
     def clean(self):
+
+        rospy.loginfo("Start cleaning")
         
         self.move_client.wait_for_server()
 
         goal = MoveBaseGoal()
         goal.target_pose.header.frame_id = self.map_topic if self.map_topic[0] != "/" else self.map_topic[1:]
 
-        while True:
+        ang = None
 
-            min_len = np.min([self.map.shape[0], self.map_cleaned.shape[0]])
+        # w_min = 0 + self.map_origin.position.x
+        # w_max = self.map_w * self.resolution + self.map_origin.position.x
 
-            free_cells = np.where((self.map[0:min_len] == COSTMAP_FREE) & (self.map_cleaned[0:min_len] == COSTMAP_FREE))
-            free_idxs = np.unravel_index(free_cells, (self.map_w,self.map_h))
+        # h_min = 0 + self.map_origin.position.y
+        # h_max = self.map_h * self.resolution + self.map_origin.position.y
+
+        map_2d = self.map.reshape(self.map_h, self.map_w)
+        a = np.where(map_2d == COSTMAP_FREE)
+
+        h_min = np.min(a[0]) * self.resolution + self.map_origin.position.x
+        h_max = np.max(a[0]) * self.resolution + self.map_origin.position.x
+
+        w_min = np.min(a[1]) * self.resolution + self.map_origin.position.y
+        w_max = np.max(a[1]) * self.resolution + self.map_origin.position.y
+
+        for h in np.arange(h_min, h_max, self.footprint_h):
+            ang = np.pi if ang == 0 else 0
+
+            steps = np.arange(w_min, w_max, self.footprint_w) if ang == 0 else np.arange(w_max, w_min, -self.footprint_w)
+
+            for w in steps:
+
+                goal.target_pose.pose.position.x = w
+                goal.target_pose.pose.position.y = h
+                quat_tf = quaternion_from_euler(0, 0, ang)
+                quat_msg = Quaternion(quat_tf[0], quat_tf[1], quat_tf[2], quat_tf[3])
+                goal.target_pose.pose.orientation = quat_msg
+
+                self.move_client.send_goal(goal)
+                wait = self.move_client.wait_for_result()
+
+                if not wait:
+                    rospy.loginfo("Goal not achieved")
+
+                if self.clean_ratio > self.clean_goal:
+                    rospy.signal_shutdown("Done")
+                    
+                self.rate.sleep()
+
+
+
+
+    # def clean_good(self):
+
+    #     rospy.loginfo("Start cleaning")
+        
+    #     self.move_client.wait_for_server()
+
+    #     goal = MoveBaseGoal()
+    #     goal.target_pose.header.frame_id = self.map_topic if self.map_topic[0] != "/" else self.map_topic[1:]
+
+    #     while True:
+
+    #         min_len = np.min([self.map.shape[0], self.map_cleaned.shape[0]])
+
+    #         free_cells = np.where((self.map[0:min_len] == COSTMAP_FREE) & (self.map_cleaned[0:min_len] == COSTMAP_FREE))
+    #         free_idxs = np.unravel_index(free_cells, (self.map_w,self.map_h))
             
-            idx = random.randrange(0, free_idxs[0].shape[1])
+    #         idx = random.randrange(0, free_idxs[0].shape[1])
             
-            x = free_idxs[0][0][idx]
-            x = x * self.resolution + self.map_origin.position.x
+    #         x = free_idxs[0][0][idx]
+    #         x = x * self.resolution + self.map_origin.position.x
 
-            y = free_idxs[1][0][idx]
-            y = y * self.resolution + self.map_origin.position.y
+    #         y = free_idxs[1][0][idx]
+    #         y = y * self.resolution + self.map_origin.position.y
 
-            goal.target_pose.pose.position.x = x
-            goal.target_pose.pose.position.y = y
-            quat_tf = quaternion_from_euler(0, 0, random.uniform(0,2*3.14))
-            quat_msg = Quaternion(quat_tf[0], quat_tf[1], quat_tf[2], quat_tf[3])
-            goal.target_pose.pose.orientation = quat_msg
+    #         # x = random.uniform(-5,5)
+    #         # y = random.uniform(-5,5)
 
-            self.move_client.send_goal(goal)
-            wait = self.move_client.wait_for_result()
+    #         goal.target_pose.pose.position.x = x
+    #         goal.target_pose.pose.position.y = y
+    #         quat_tf = quaternion_from_euler(0, 0, random.uniform(0,2*3.14))
+    #         quat_msg = Quaternion(quat_tf[0], quat_tf[1], quat_tf[2], quat_tf[3])
+    #         goal.target_pose.pose.orientation = quat_msg
 
-            if not wait:
-                pass
+    #         self.move_client.send_goal(goal)
+    #         wait = self.move_client.wait_for_result()
 
-            if self.clean_ratio > 0.95:
-                rospy.signal_shutdown("Done")
+    #         if not wait:
+    #             pass
+
+    #         if self.clean_ratio > 0.95: # TODO
+    #             rospy.signal_shutdown("Done")
     
-            self.rate.sleep()
+    #         self.rate.sleep()
 
+    # def clean_1(self):
+
+    #     rospy.loginfo("Start cleaning")
+        
+    #     self.move_client.wait_for_server()
+
+    #     goal = MoveBaseGoal()
+    #     goal.target_pose.header.frame_id = self.map_topic if self.map_topic[0] != "/" else self.map_topic[1:]
+
+    #     tfBuffer = tf2_ros.Buffer()
+    #     listener = tf2_ros.TransformListener(tfBuffer)
+
+    #     while not rospy.is_shutdown():
+
+    #         try: # TODO params frames
+    #             trans = tfBuffer.lookup_transform('map', 'base_footprint', rospy.Time())
+    #         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+    #             self.rate.sleep()
+    #             continue
+
+    #         # TODO params
+    #         x = random.uniform(-2,2)
+    #         y = random.uniform(-2,2)
+    #         ang = random.uniform(-3.14, 3.14)
+    #         quat_tf = quaternion_from_euler(0, 0, ang)
+    #         quat = Quaternion(quat_tf[0], quat_tf[1], quat_tf[2], quat_tf[3])
+
+
+    #         goal.target_pose.pose.position.x = x
+    #         goal.target_pose.pose.position.y = y
+    #         goal.target_pose.pose.orientation = quat
+            
+    #         goal.target_pose = tf2_geometry_msgs.do_transform_pose(goal.target_pose, trans)
+
+    #         self.move_client.send_goal(goal)
+    #         wait = self.move_client.wait_for_result()
+
+    #         if not wait:
+    #             pass
+
+    #         if self.clean_ratio > 0.95: # TODO
+    #             rospy.signal_shutdown("Done")
+    
+    #         self.rate.sleep()
 
 """
 FUNCTIONS DEFINITIONS
@@ -146,7 +256,7 @@ if __name__ == '__main__':
     try:
         bot = CleanerBot()
         bot.config()
-        bot.get_map_details()
+        bot.get_infos()
         bot.clean()
 
         rospy.spin()
